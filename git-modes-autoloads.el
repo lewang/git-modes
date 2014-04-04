5fda4af6178379975aa736c50771cdf42a9cc81f
Le Wang <le.wang@agworld.com.au>
2014-04-04 10:56:21 -0400

    update autoloads.

--- git-modes-autoloads.el
+++ git-modes-autoloads.el
@@ -4,7 +4,7 @@
 
 
 ;;;### (autoloads (git-commit-mode) "git-commit-mode" "git-commit-mode.el"
-;;;;;;  (20971 46516 0 0))
+;;;;;;  (21310 51018 0 0))
 ;;; Generated autoloads from git-commit-mode.el
 
 (autoload 'git-commit-mode "git-commit-mode" "\
@@ -16,14 +16,44 @@ basic structure of and errors in git commit messages.
 
 \(fn)" t nil)
 
-(eval-after-load 'magit #'(progn (define-derived-mode magit-log-edit-mode git-commit-mode "Magit Log Edit" (set (make-local-variable 'git-commit-commit-function) (apply-partially #'call-interactively 'magit-log-edit-commit))) (substitute-key-definition 'magit-log-edit-toggle-signoff 'git-commit-signoff magit-log-edit-mode-map) (substitute-key-definition 'magit-log-edit-commit 'git-commit-commit magit-log-edit-mode-map)))
-
 (dolist (pattern '("/COMMIT_EDITMSG\\'" "/NOTES_EDITMSG\\'" "/MERGE_MSG\\'" "/TAG_EDITMSG\\'" "/PULLREQ_EDITMSG\\'")) (add-to-list 'auto-mode-alist (cons pattern 'git-commit-mode)))
 
 ;;;***
 
+;;;### (autoloads (git-rebase-mode) "git-rebase-mode" "git-rebase-mode.el"
+;;;;;;  (21310 51018 0 0))
+;;; Generated autoloads from git-rebase-mode.el
+
+(autoload 'git-rebase-mode "git-rebase-mode" "\
+Major mode for editing of a Git rebase file.
+
+Rebase files are generated when you run 'git rebase -i' or run
+`magit-interactive-rebase'.  They describe how Git should perform
+the rebase.  See the documentation for git-rebase (e.g., by
+running 'man git-rebase' at the command line) for details.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("/git-rebase-todo\\'" . git-rebase-mode))
+
+;;;***
+
+;;;### (autoloads (gitattributes-mode) "gitattributes-mode" "gitattributes-mode.el"
+;;;;;;  (21310 51018 0 0))
+;;; Generated autoloads from gitattributes-mode.el
+
+(autoload 'gitattributes-mode "gitattributes-mode" "\
+A major mode for editing .gitattributes files.
+\\{gitattributes-mode-map}
+
+\(fn)" t nil)
+
+(dolist (pattern '("/\\.gitattributes\\'" "/\\.git/info/attributes\\'" "/git/attributes\\'")) (add-to-list 'auto-mode-alist (cons pattern #'gitattributes-mode)))
+
+;;;***
+
 ;;;### (autoloads (gitconfig-mode) "gitconfig-mode" "gitconfig-mode.el"
-;;;;;;  (20971 46516 0 0))
+;;;;;;  (21310 51018 0 0))
 ;;; Generated autoloads from gitconfig-mode.el
 
 (autoload 'gitconfig-mode "gitconfig-mode" "\
@@ -31,12 +61,12 @@ A major mode for editing .gitconfig files.
 
 \(fn)" t nil)
 
-(dolist (pattern '("/\\.gitconfig\\'" "/\\.git/config\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitconfig-mode)))
+(dolist (pattern '("/\\.gitconfig\\'" "/\\.git/config\\'" "/git/config\\'" "/\\.gitmodules\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitconfig-mode)))
 
 ;;;***
 
 ;;;### (autoloads (gitignore-mode) "gitignore-mode" "gitignore-mode.el"
-;;;;;;  (20971 46516 0 0))
+;;;;;;  (21310 51018 0 0))
 ;;; Generated autoloads from gitignore-mode.el
 
 (autoload 'gitignore-mode "gitignore-mode" "\
@@ -44,7 +74,7 @@ A major mode for editing .gitignore files.
 
 \(fn)" t nil)
 
-(dolist (pattern '("/\\.gitignore\\'" "/\\.git/info/exclude\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitignore-mode)))
+(dolist (pattern (list "/\\.gitignore\\'" "/\\.git/info/exclude\\'" "/git/ignore\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitignore-mode)))
 
 ;;;***
 
